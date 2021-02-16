1b20bfe625915fe74f52d29b9b11e7d65a885024
Jacin <jacinjiyan@gmail.com>
2021-02-17 03:34:23 +1100

    introduced useMemo

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,16 +1,39 @@
-import React from 'react'
+import React, { useState, useMemo } from 'react'
 import { formatPrice } from 'utils/formatPrice'
+import axios from 'config/axios'
 
-const CartItem = ({ cart: { name, image, price, quantity } = {} }) => {
+const CartItem = ({ cart = {}, cart: { id, name, image, price, quantity: initQuantity }, updateCart, deleteCartUpdate }) => {
 
-    const handleChange = () => {
-        
+    const [quantity, setQuantity] = useState(initQuantity)
+
+    const subTotal = useMemo(() => {
+      return formatPrice(quantity * parseInt(price))
+    },[quantity, price])
+
+    const handleQuantity = e => {
+        const _quantity = parseInt(e.target.value)
+        setQuantity(_quantity)
+        const newCart = {
+            ...cart,
+            quantity: _quantity
+        }
+        axios.put(`/carts/${id}`, newCart)
+            .then(() => {
+                updateCart(newCart)
+            })
+    }
+
+    const deleteCart = () => {
+        axios.delete(`/carts/${id}`)
+        .then(() => {
+            deleteCartUpdate(cart)
+        })
     }
 
     return (
         <div className="columns is-vcentered">
             <div className="column is-1 ">
-                <span className="close">x</span>
+                <span className="close" onClick={deleteCart}>x</span>
             </div>
             <div className="column is-3 is-offset-1">
                 <img src={image} alt={name} width="150" />
@@ -22,15 +45,16 @@ const CartItem = ({ cart: { name, image, price, quantity } = {} }) => {
                 <span className="price">{formatPrice(price)}</span>
             </div>
             <div className="column is-2">
-                <input 
-                type="number" 
-                className="input num-input" 
-                value={quantity} 
-                onChange={handleChange} 
+                <input
+                    type="number"
+                    className="input num-input"
+                    value={quantity}
+                    min={1}
+                    onChange={handleQuantity}
                 />
             </div>
             <div className="column is-1">
-                <span className="sub-total">{formatPrice(quantity * parseInt(price))}</span>
+                <span className="sub-total">{subTotal}</span>
             </div>
         </div>
     )
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,8 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useMemo } from 'react'
 import Layout from 'Layout'
 import CartItem from 'components/CartItem'
 import axios from 'config/axios'
 import { formatPrice } from 'utils/formatPrice'
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
 
 const Cart = () => {
 
@@ -19,22 +21,40 @@ const Cart = () => {
             })
     }, [])
 
-    const totalPrice = () => carts
-                            .map(cart => cart.quantity * parseInt(cart.price))
-                            .reduce((prev, curr) => prev + curr, 0)
+    const totalPrice = useMemo(() => () => carts
+                                        .map(cart => cart.quantity * parseInt(cart.price))
+                                        .reduce((prev, curr) => prev + curr, 0), [carts])
 
+    const updateCart = cart => {
+        const newCarts = [...carts]
+        const _index = newCarts.findIndex(newCart => newCart.id === cart.id)
+        newCarts.splice(_index, 1, cart)
+        setCarts(newCarts)
+    }
+
+    const deleteCartUpdate = cart => {
+        const _carts = carts.filter(_cart => _cart.id !== cart.id)
+        setCarts(_carts)
+    }
 
     return (
         < Layout >
             <div className="cart-screen">
                 <span className="cart-title">Your Cart</span>
                 <div className="cart-list">
-                    {
-                        carts.map(cart =>
-                            <CartItem key={cart.id} cart={cart} />
-                        )
-                    }
+                    <TransitionGroup component={null}>
+                        {
+                            carts.map(cart =>
+                                <CSSTransition classNames="cart-item" timeout={300} key={cart.id}>
+                                    <CartItem key={cart.id} cart={cart} updateCart={updateCart} deleteCartUpdate={deleteCartUpdate} />
+                                </CSSTransition>
+                            )
+                        }
+                    </TransitionGroup>
                 </div>
+                {
+                    carts.length === 0 ? <p className="no-cart">No ITEMS</p> : ''
+                }
                 <div className="cart-total">
                     Total:
                     <span className="total-price">{formatPrice(totalPrice())}</span>
