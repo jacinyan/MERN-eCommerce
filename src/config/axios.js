cba1ccb05305a0484bde5f52ba43938358af231b
Jacin <jacinjiyan@gmail.com>
2021-02-20 22:01:53 +1100

    env endpoint

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,7 +2,7 @@ import _axios from 'axios'
 
 const axios = baseURL => {
     const instance = _axios.create({
-        baseURL: baseURL || 'http://localhost:9090',
+        baseURL: baseURL || process.env.REACT_APP_API_ENDPOINT || 'http://localhost:9090',
         timeout: 1000,
     });
 
